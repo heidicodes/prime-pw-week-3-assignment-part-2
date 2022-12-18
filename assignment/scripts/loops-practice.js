ddf7884407000c69ea4ef14a3f57718b360b7865
Heidi Ganac Nicholson <heidiganac@gmail.com>
2022-12-18 12:34:19 -0600

    created for loop to log numbers 3 to 5 with use of break

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -20,6 +20,12 @@ for (let i=0; i<6; i++){ // for loop setting variable i and condition
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for (let i=3; i<=5; i++){
+  if ( i == 6 ){
+    break; // break if i = 6, will not iterate
+  }
+  console.log('For loop between 3 and 5', i);
+} // loops through 3 and 5 and prints these numbers
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
